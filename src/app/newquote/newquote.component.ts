437e3966dd031d2ce74237b1d2e7716c3d9eb648
barkotenicholas <barkotenicholas@gmail.com>
2022-02-04 22:11:57 +0300

    moved html code from mainapp.html to newquote.html

--- src/app/newquote/newquote.component.ts
+++ src/app/newquote/newquote.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-newquote',
+  selector: 'newquote',
   templateUrl: './newquote.component.html',
   styleUrls: ['./newquote.component.css']
 })
