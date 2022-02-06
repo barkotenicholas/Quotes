f5ba971a111b591740f5a9aa22f36a404a71fbf2
barkotenicholas <barkotenicholas@gmail.com>
2022-02-06 21:30:45 +0300

    Added finishing touches

--- src/app/mostvotes.directive.ts
+++ src/app/mostvotes.directive.ts
@@ -6,22 +6,9 @@ import { Quote } from './quote';
 })
 export class MostvotesDirective {
 
-  @Input() set showMostLiked(quotes:Quote[]){
-    let largets: Quote;
-    let z =0
-    let i = 0 ;
-    for(i;i<quotes.length;i++){
-      if(quotes[i].upvote>z){
-        largets = quotes[i];
-        z = quotes[i].upvote;
-      }
-    }
-    if(z>1){
-      this.elm.nativeElement.style.textDecoration="line-through";
-    }
-  }
 
   constructor(private elm:ElementRef) {
+    this.elm.nativeElement.style.fontStyle = 'italic';
    }
 
    ngOnInit(){
